2b4261ced3f56b5f8554f6cc5d505be77b9173ea
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-09-14 00:13:39 +0900

    style: return Type 세팅

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   @Get('/')
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boardsService.getAllBoards();
   }
 }
