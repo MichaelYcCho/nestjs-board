6c6c60a9ed3b5565a7629c52c8627ecc042b2895
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-09-17 22:37:00 +0900

    feat: throw Exception Service

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -12,7 +12,13 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+
+    return found;
   }
 
   createBoard(createBoardDto: CreateBoardDto): Board {
@@ -35,7 +41,9 @@ export class BoardsService {
   }
 
   deleteBoardById(id: string): void {
+    const found = this.getBoardById(id);
+
     // id가 같지 않은 board만 남기고 다시 할당
-    this.boards = this.boards.filter((board) => board.id !== id);
+    this.boards = this.boards.filter((board) => board.id !== found.id);
   }
 }
