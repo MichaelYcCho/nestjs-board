7f8db1aad21f892c518ff8b2b0abbe9728be33b3
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-09-23 13:58:57 +0900

    feat: Delete Board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
@@ -24,32 +25,8 @@ export class BoardsController {
     return this.boardsService.createBoard(createBoardDto);
   }
 
-  // @Get('/')
-  // getAllBoards(): Board[] {
-  //   return this.boardsService.getAllBoards();
-  // }
-
-  // @Get('/:id')
-  // getBoardById(@Param('id') id: string): Board {
-  //   return this.boardsService.getBoardById(id);
-  // }
-
-  // @Post()
-  // @UsePipes(ValidationPipe)
-  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-  //   return this.boardsService.createBoard(createBoardDto);
-  // }
-
-  // @Patch('/:id/status')
-  // updateBoardStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', BoardStausValidationPipe) status: BoardStatus,
-  // ): Board {
-  //   return this.boardsService.updateBoardStatus(id, status);
-  // }
-
-  // @Delete('/:id')
-  // deleteBoardById(@Param('id') id: string): void {
-  //   this.boardsService.deleteBoardById(id);
-  // }
+  @Delete('/:id')
+  deleteBoard(@Param('id', ParseIntPipe) id: number) {
+    return this.boardsService.deleteBoard(id);
+  }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -25,36 +25,12 @@ export class BoardsService {
     return this.boardRepository.createBoard(createBoardDto);
   }
 
-  // private boards: Board[] = [];
-  // getAllBoards(): Board[] {
-  //   return this.boards;
-  // }
-  // getBoardById(id: string): Board {
-  //   const found = this.boards.find((board) => board.id === id);
-  //   if (!found) {
-  //     throw new NotFoundException(`Can't find Board with id ${id}`);
-  //   }
-  //   return found;
-  // }
-  // createBoard(createBoardDto: CreateBoardDto): Board {
-  //   const { title, description } = createBoardDto;
-  //   const board: Board = {
-  //     id: uuid(),
-  //     title,
-  //     description,
-  //     status: BoardStatus.PUBLIC,
-  //   };
-  //   this.boards.push(board);
-  //   return board;
-  // }
-  // updateBoardStatus(id: string, status: BoardStatus): Board {
-  //   const board = this.getBoardById(id);
-  //   board.status = status;
-  //   return board;
-  // }
-  // deleteBoardById(id: string): void {
-  //   const found = this.getBoardById(id);
-  //   // id가 같지 않은 board만 남기고 다시 할당
-  //   this.boards = this.boards.filter((board) => board.id !== found.id);
-  // }
+  async deleteBoard(id: number): Promise<void> {
+    const result = await this.boardRepository.delete({ id });
+    if (result.affected === 0) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+
+    console.log('result', result);
+  }
 }
