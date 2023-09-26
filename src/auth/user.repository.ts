2b5c2f8cf5eb77f803db12da1d6b58a7037eacc4
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-09-26 23:50:40 +0900

    feat: User Repository - CreateUser

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,10 +5,33 @@ import {
 } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(createMemberDto: AuthCredentialsDto): Promise<void> {
+    try {
+      const { username, password } = createMemberDto;
+
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const member = await this.create({
+        username,
+        password: hashedPassword,
+      });
+      await this.save(member);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
