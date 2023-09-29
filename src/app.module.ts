0c59e9febe15ef3f4250abcc59324226b5ae8c25
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-09-29 12:01:33 +0900

    feat: SignIn with JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { BoardsModule } from './boards/boards.module';
-import { BoardsController } from './boards/boards.controller';
-import { BoardsService } from './boards/boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getTypeOrmConfig } from './configs/typeorm.config';
 import { nestConfig } from './configs/config';
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,24 +3,29 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UsersRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
-    console.log('username', username);
     const user = await this.usersRepository.findOne({ where: { username } });
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'Login successful';
+      const payload = { username };
+      const accessToken = this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('Invalid credentials');
     }
