138ca1c598fcc10cf09a267a24edc824c2bb0fe8
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-09-30 23:56:17 +0900

    test: JWT Test

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { UsersRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -17,8 +18,8 @@ import { PassportModule } from '@nestjs/passport';
   ],
   controllers: [AuthController],
   // JWT Strategy를 해당 Auth 모듈에서 사용할수 있게 등록
-  providers: [AuthService, UsersRepository],
+  providers: [AuthService, UsersRepository, JwtStrategy],
   // JWT Strategy, PassportModule을 다른 모듈에서 사용할수 있게 등록
-  exports: [PassportModule, JwtModule],
+  exports: [JwtStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
